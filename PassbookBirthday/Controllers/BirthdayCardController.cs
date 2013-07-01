321e6c637d8f9710be7799a0079070a09a8234c7
Tomas McGuinness <tomas@chewedpencil.net>
2013-07-01 17:20:54 +0100

    Solution files

---
+++ PassbookBirthday/Controllers/BirthdayCardController.cs
@@ -0,0 +1,29 @@
+﻿using PassbookBirthday.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace PassbookBirthday.Controllers
+{
+    public class BirthdayCardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(BirthdayCardModel card)
+        {
+            return View();
+        }
+    }
+}
