e259d2fe50cb44d9b1c067a9f7afd38ec55a7593
Tomas McGuinness <tomas@chewedpencil.net>
2013-07-04 16:56:40 +0100

    Trying to make Birthday card create async

--- PassbookBirthday/Controllers/BirthdayCardController.cs
+++ PassbookBirthday/Controllers/BirthdayCardController.cs
@@ -1,9 +1,11 @@
 ﻿using PassbookBirthday.Models;
+using PassVerse.Management.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace PassbookBirthday.Controllers
@@ -21,11 +23,18 @@ namespace PassbookBirthday.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(BirthdayCardModel card)
+        public async Task<ActionResult> Create(BirthdayCardModel card)
         {
             using (HttpClientHandler handler = new HttpClientHandler() { Credentials = new NetworkCredential() })
             {
                 HttpClient client = new HttpClient(handler);
+
+                PassModel pass = new PassModel();
+
+                HttpResponseMessage resp = await client.PutAsJsonAsync<PassModel>("https://passverseapi.azurewebsites.net/api/v1/pass/e5bea643-0a63-4162-a753-c39f0b0c3c4c", pass);
+
+
+
                 // Create a new pass and email it.
                 //
                 return View();
